c6a965798386bd0f6c13f469134eda9736c8e39a
Baangkok Vanijyananda <iambaangkok@gmail.com>
2021-01-05 14:24:22 +0700

    Lab8_2 completed

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 using namespace std;
 
+
+
 //Write the function printO() here
+void printO(long long int row, long long int col){
+
+	if(row < 1 or col < 1){
+		cout << "Invalid input";
+		return;
+	}
+
+	for(int i = 0 ; i < row; ++i){
+		for(int j = 0 ; j < col; ++j){
+			cout << 'O';
+		}cout << endl;
+	}
+
+	return;
+}
+
 
 int main(){
 	
